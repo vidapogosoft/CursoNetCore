2031e36fb1d8badcd3d22a3cf622209721779f9d
vidapogosoft <vidapogosoft@hotmail.com>
2021-04-14 22:07:17 -0500

    talleres dias 7 y 8
    JW Token
    DTo y services
    Middlewares

---
+++ TokenApi/TokenApi/Repositories/ProductsRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using TokenApi.Models;
+using TokenApi.DTos;
+using AutoMapper;
+
+
+namespace TokenApi.Repositories
+{
+    public class ProductsRepository : RepositoryBase<Productos>, IProductsRepository
+    {
+
+        private readonly IMapper _mapper;
+
+        public ProductsRepository(AppDbContext repositoryContext, IMapper mapper)
+           : base(repositoryContext)
+        {
+            _mapper = mapper;
+        }
+
+        void IProductsRepository.CreateProduct(ProductsDto product)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IProductsRepository.DeleteProduct(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<ProductsDto>> IProductsRepository.GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ProductsDto> IProductsRepository.GetProductByIdAsync(Guid ProdcuctId)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IProductsRepository.UpdateProduct(Guid id, ProductsDto product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
